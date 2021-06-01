ab59cd15ae57ab293a2d194014eaefc2ac4f3eb6
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-01 16:31:31 -0300

    Add files via upload

---
+++ Davi_250521.py
@@ -0,0 +1,56 @@
+name = "Palmeiras"
+print (name)
+
+name1 = name + " " + name + " " + name + " " + name
+print (name1)
+
+name2 = "Igor"
+print (name2)
+
+age = 6
+print (age)
+
+print (name2+str(age))
+
+print (name2+str(age)+"!")
+
+print (name2+" "+str(age)+"!")
+
+#Question 10
+print (1+2*3)
+
+print ((1+2)*3)
+
+print ("Davi\nMotta")
+
+print ("  Davi\nMotta")
+
+name3 = "  japonesa  "
+print (name3+"!")
+print (name3.strip()+"!")
+name4 = "  japonesa  !"
+print (name4)
+
+print (name4.strip())
+
+print ("\\tem")
+
+print (164/34)
+
+print (18*12)
+
+name5 = "Davi"
+print (name5)
+
+name6 = "Motta"
+print (name6)
+
+name7 = name5 + " " + name6
+print (name7)
+
+name8 = name5 + " " + name6
+print (name8.lower())
+
+name9 = name5 + " " + name6
+print (name9.title())
+
